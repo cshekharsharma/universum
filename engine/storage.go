e116efb4fc2fcf43d7ceb588b6042b6630e464fa
Chandra Shekhar Sharma <shekharsharma705@gmail.com>
2024-03-31 20:34:28 +0530

    implemented Exists, and Delete commands

--- engine/storage.go
+++ engine/storage.go
@@ -1,6 +1,7 @@
 package engine
 
 import (
+	"universum/consts"
 	"universum/engine/entity"
 	"universum/utils"
 	"unsafe"
@@ -19,24 +20,45 @@ func (s *Storage) Initialize() {
 	keypool = make(map[string]unsafe.Pointer)
 }
 
+func (s *Storage) Exists(key string) (bool, uint32) {
+	pointer, ok := keypool[key]
+	if !ok {
+		return false, consts.CRC_RECORD_NOT_FOUND
+	}
+
+	record, ok := memoryStore[pointer]
+	if ok {
+		expired := s.hasExpired(record)
+
+		if !expired {
+			return true, consts.CRC_RECORD_FOUND
+		} else {
+			deleteByPointer(record, pointer)
+			return false, consts.CRC_RECORD_EXPIRED
+		}
+	}
+
+	return false, consts.CRC_RECORD_NOT_FOUND
+}
+
 func (s *Storage) Get(key string) (*entity.Record, uint32) {
 	pointer, ok := keypool[key]
 	if !ok {
-		return nil, RESPONSECODE_KEY_NOT_FOUND
+		return nil, consts.CRC_RECORD_NOT_FOUND
 	}
 
 	record := memoryStore[pointer]
 	if record == nil {
-		return nil, RESPONSECODE_KEY_NOT_FOUND
+		return nil, consts.CRC_RECORD_NOT_FOUND
 	}
 
 	if s.hasExpired(record) {
-		DeleteByPointer(record, pointer)
-		return nil, RESPONSECODE_KEY_EXPIRED
+		deleteByPointer(record, pointer)
+		return nil, consts.CRC_RECORD_EXPIRED
 	}
 
 	record.LastAccessedAt = utils.GetCurrentEPochTime()
-	return record, RESPONSECODE_RECORD_FOUND
+	return record, consts.CRC_RECORD_FOUND
 }
 
 func (s *Storage) Set(key string, value interface{}, ttl uint64) (bool, uint32) {
@@ -60,21 +82,40 @@ func (s *Storage) Set(key string, value interface{}, ttl uint64) (bool, uint32) 
 		expirationMap[record] = uint64(utils.GetCurrentEPochTime()) + ttl
 	}
 
-	return true, RESPONSECODE_UPDATED
+	return true, consts.CRC_UPDATED
 }
 
-func (s *Storage) Delete() *entity.Record { return nil }
+func (s *Storage) Delete(key string) (bool, uint32) {
+	if exists, _ := s.Exists(key); !exists {
+		return true, consts.CRC_RECORD_DELETED
+	}
 
-func DeleteByPointer(record *entity.Record, pointer unsafe.Pointer) bool {
+	pointer := keypool[key]
+	record := memoryStore[pointer]
+
+	deleted := deleteByPointer(record, pointer)
+
+	if deleted {
+		return true, consts.CRC_RECORD_DELETED
+	}
+
+	return false, consts.CRC_RECORD_NOT_DELETED
+}
+
+// /////////////////////// Internal Functions ///////////////////////////
+func (s *Storage) hasExpired(record *entity.Record) bool {
+	expiry, ok := expirationMap[record]
+	if !ok {
+		return false
+	}
+
+	return expiry < uint64(utils.GetCurrentEPochTime())
+}
+
+func deleteByPointer(record *entity.Record, pointer unsafe.Pointer) bool {
 	delete(memoryStore, pointer)
 	delete(keypool, *((*string)(pointer)))
 	delete(expirationMap, record)
 
 	return true
-}
-
-func (s *Storage) Exists() *entity.Record { return nil }
-
-func (s *Storage) hasExpired(record *entity.Record) bool {
-	return false
 }
