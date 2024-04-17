efc5ee55e0bf17c3bc0b40c81579c47f26e0b249
Chandra Shekhar Sharma <shekharsharma705@gmail.com>
2024-04-17 19:54:14 +0530

    minor fixes

--- storage/interface.go
+++ storage/interface.go
@@ -8,4 +8,9 @@ type DataStore interface {
 	Delete(key string) (bool, uint32)
 	IncrDecrInteger(key string, offset int64, isIncr bool) (int64, uint32)
 	Append(key string, value string) (int64, uint32)
+	Mget(keys []string) (map[string]interface{}, uint32)
+	MSet(kvMap map[string]interface{}) (map[string]interface{}, uint32)
+	MDelete(keys []string) (map[string]interface{}, uint32)
+	TTL(key string) (int64, uint32)
+	Expire(key string, ttl int64) (bool, uint32)
 }
